37aed4c022a5f282a2d4a61864b16adb984af336
capJavert <ante@kickass.website>
2023-04-19 04:12:12 +0200

    feat: add query devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { DataServiceProvider } from '@kickass-coderz/data-service'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AppProps } from 'next/app'
 
 import { beerService } from '../BeerService'
@@ -7,6 +8,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     return (
         <DataServiceProvider dataService={beerService}>
             <Component {...pageProps} />
+            <ReactQueryDevtools />
         </DataServiceProvider>
     )
 }
